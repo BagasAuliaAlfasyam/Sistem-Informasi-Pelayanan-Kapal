3c14af7d54bdddde6fb8730e440040e0e955afd5
bryankazuro04 <mhdalana211201@gmail.com>
2023-08-10 04:37:25 +0700

    update model

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
-    protected $table = 'jadwals';
+    protected $table = 'schedules';
     protected $guarded = ['id'];
 
 }
