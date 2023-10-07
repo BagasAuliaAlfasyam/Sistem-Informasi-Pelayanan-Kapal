bebe5012d5887eb75588ad26099fcf15a4f7b71f
bryankazuro04 <mhdalana211201@gmail.com>
2023-10-08 02:32:59 +0700

    memperbaiki id_kapal yang tidak cocok dan menambah notifikasi error

--- resources/views/pages/kapal/rincian/add.blade.php
+++ resources/views/pages/kapal/rincian/add.blade.php
@@ -1,4 +1,13 @@
 <x-app-layout>
+  @error('id_kapal')
+  <div class="alert">
+    <div class="text-error flex mb-1">
+      <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg> &ThickSpace;
+      <span>Rincian untuk id kapal ini sudah terisi! Periksa kembali kapal dengan id yang berbeda</span>
+    </div>
+  </div>
+  @enderror
+
   <div class="px-4 sm:px-6 lg:px-8 pt-4 w-full max-w-9xl relative mx-auto">
     <h1 class="font-bold my-4 text-3xl">Penambahan Rincian Data Kapal</h1>
     <a href="{{ route('details.index') }}" class="absolute right-0 top-0 pt-8 pr-8">
@@ -12,7 +21,7 @@
         <select class="select select-bordered @error ('nama_kapal') select-error @enderror w-full bg-slate-800" id="kapal" name="nama_kapal" required autofocus>
           <option value="" selected disabled></option>
           @foreach ($ships as $kapal)
-            <option value="{{ $kapal->nama_kapal }}">{{ $kapal->nama_kapal }}</option>
+            <option value="{{ $kapal->nama_kapal }}">{{ $kapal->id }} ~ &ThickSpace; {{ $kapal->nama_kapal }}</option>
           @endforeach
         </select>
       </div>
@@ -26,31 +35,27 @@
       <input type="hidden" name="id_kapal" value="" class="kapal-input">
 
       <script>
+        // Buat objek untuk menyimpan ID kapal berdasarkan nama kapal
+        const shipIdMap = {};
+        @foreach ($ships as $kapal)
+          shipIdMap["{{ $kapal->nama_kapal }}"] = "{{ $kapal->id }}";
+        @endforeach
+
         // Ambil elemen-elemen yang diperlukan
         const kapalSelect = document.getElementById('kapal');
         const kapalInput = document.querySelector('.kapal-input');
-      
+
         // Tambahkan event listener saat terjadi perubahan pada select
         kapalSelect.addEventListener('change', function () {
           // Ambil nilai yang dipilih (nama kapal)
           const selectedValue = kapalSelect.value;
-          
-          // Cari ID yang sesuai dengan nama kapal yang dipilih
-          const selectedId = findSelectedId(selectedValue);
-          
+
+          // Cari ID yang sesuai dengan nama kapal yang dipilih dalam objek shipIdMap
+          const selectedId = shipIdMap[selectedValue];
+
           // Set nilai input dengan ID yang sesuai
           kapalInput.value = selectedId;
         });
-      
-        // Fungsi untuk mencari ID berdasarkan nama kapal
-        function findSelectedId(selectedName) {
-          @foreach ($ships as $kapal)
-            if ("{{ $kapal->nama_kapal }}" === selectedName) {
-              return "{{ $kapal->id }}";
-            }
-          @endforeach
-          return "";
-        }
       </script>
 
       <div class="mb-4">
