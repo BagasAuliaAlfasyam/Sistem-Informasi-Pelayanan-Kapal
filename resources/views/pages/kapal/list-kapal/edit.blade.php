608672883a9d437bd8a19fd664ede0bf7c432e47
BagasAuliaAlfasyam <bagas.190180087@mhs.unimal.ac.id>
2024-01-08 17:27:06 +0700

    on null bongkar fix

--- resources/views/pages/kapal/list-kapal/edit.blade.php
+++ resources/views/pages/kapal/list-kapal/edit.blade.php
@@ -82,9 +82,9 @@
         <label for="pemilik" class="block mb-3">Pemilik</label>
         <select class="select select-bordered @error ('pemilik') select-error @enderror w-full bg-slate-800" id="pemilik" name="pemilik" required>
           <option value="1" {{ $kapal->pemilik == 1 ? 'selected' : '' }}>Pemilik</option>
-          <option value="0" {{ $kapal->pemilik == 0 ? 'selected' : '' }}>Non-Pemilik</option>
+          <option value="0" {{ $kapal->pemilik == 0 ? 'selected' : '' }}>Non-Pemilik</option>>
         </select>
-      </div>
+      </div>Pemilik
       @error ('pemilik')
         <div class="text-error flex mb-1">
           <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
