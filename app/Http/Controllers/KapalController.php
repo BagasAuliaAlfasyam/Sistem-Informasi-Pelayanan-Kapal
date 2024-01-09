01392219c0c7f7d2b7f1a9e7e17e824582db142c
bryankazuro04 <mhdalana211201@gmail.com>
2024-01-09 19:07:28 +0700

    fixed recapitulation function for data empty

--- app/Http/Controllers/KapalController.php
+++ app/Http/Controllers/KapalController.php
@@ -108,17 +108,6 @@ class KapalController extends Controller
     public function recapitulation()
     {
         $ships = Kapal::paginate(10)->withPath(route('rekapitulasi.index'));
-        $kapal = Kapal::all();
-        $penjadwalan = Schedule::all();
-        if (count($kapal) != count($penjadwalan)) {
-            return redirect()
-                ->route('kapal.index')
-                ->with([
-                    'message' => 'Data kapal belum lengkap',
-                    'alert-type' => 'error',
-                    'icon' => '<svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>',
-                ]);
-        }
         
         return view('pages.kapal.rekapitulasi-data.index', compact('ships'));
     }
